16c1cd9f451e52c49219d16202dcda0af59f8c36
ricacnsg <ricamaegconsigo@gmail.com>
2026-07-07 10:07:05 +0800

    Added an initial navigation behavior in js file

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//behavior for start button
 const myButton = document.getElementById("startButton");
 const message = document.getElementById("message");
 myButton.addEventListener("click", () => {  message.textContent = "Goodluck to your Lab Exam!";});
+
+//behavior for navigations
+const links = document.querySelectorAll('.nav-links a');
+        links.forEach(link => {
+            link.addEventListener('click', function() {
+                document.querySelector('.nav-links a.active').classList.remove('active');
+                this.classList.add('active');
+            });
+        });
