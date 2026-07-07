a655e87d59244d6a23bd4d367277f338aee2b6b7
ricacnsg <ricamaegconsigo@gmail.com>
2026-07-07 09:42:10 +0800

    Added a button and its behavior in js file

---
+++ script.js
@@ -0,0 +1,3 @@
+const myButton = document.getElementById("startButton");
+const message = document.getElementById("message");
+myButton.addEventListener("click", () => {  message.textContent = "Goodluck to your Lab Exam!";});
